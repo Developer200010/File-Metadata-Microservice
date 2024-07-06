1c77abd626ba1c2e97d9785d1aa34e05a0b72456
chandan vishwakarma <chandanvishw11002@gmail.com>
2024-07-07 01:45:48 +0530

    done

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require("multer");
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+const upload = multer({dist: './public/data/uploads'});
+
+app.post("/api/fileanalyse",upload.single('upfile'), (req,res)=>{
+  let{originalname, mimetype, size} = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size : size
+
+  })
+})
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
